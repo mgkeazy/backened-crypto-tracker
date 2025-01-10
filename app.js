3e9fc2a86cb96bd6a03f01637cab4e69974b368e
Khushal Kumawat <khushalgc143@gmail.com>
2025-01-10 22:19:20 +0530

    Third Commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ cron.schedule('0 */2 * * *', async () => {
 
 app.use('/api/crypto',cryptoRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
